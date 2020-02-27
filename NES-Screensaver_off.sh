9fed32f848bbfdfd50d20a1d68947803dae0d7ea
mrchrisster <60834906+mrchrisster@users.noreply.github.com>
2020-02-26 23:12:49 -0800

    Create NES-Screensaver_off.sh

---
+++ NES-Screensaver_off.sh
@@ -0,0 +1,19 @@
+if [ "$(uname -n)" != "MiSTer" ]
+then
+        echo "This script must be run"
+        echo "on a MiSTer system."
+        exit 1
+fi
+
+mount | grep -q "on / .*[(,]ro[,$]" && RO_ROOT="true"
+[ "$RO_ROOT" == "true" ] && mount / -o remount,rw
+mv /etc/init.d/S95screensaver /etc/init.d/_S95screensaver > /dev/null 2>&1
+sync
+[ "$RO_ROOT" == "true" ] && mount / -o remount,ro
+echo "Restoring boot.rom"
+cp /media/fat/Games/NES/boot1.rom.bak /media/fat/Games/NES/boot1.rom
+rm /media/fat/Games/NES/boot1.rom.bak
+
+echo "Screensaver is off and"
+echo "inactive at startup."
+echo "Done!"
