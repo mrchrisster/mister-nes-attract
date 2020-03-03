a02b9c59c599f558db4aa5f6f3e1c9f74a5a63b0
woelper <woelper@gmail.com>
2020-03-03 22:21:54 +0100

    boot rom and sfc

--- saver.py
+++ saver.py
@@ -6,10 +6,15 @@ def extract_random(folder, start_pattern):
     for rom_archive in os.listdir(folder):
         if rom_archive.startswith(start_pattern):
             zip = zipfile.ZipFile(rom_archive, 'r')
-            # only .sfc
+            
+            # filtering: list comprehension
+            only_sfc = [f for f in zip.namelist() if f.endswith("sfc")]
+            
+            # uncomment next for only .sfc
+            # random_zip_member = random.choice(only_sfc)
             random_zip_member = random.choice(zip.namelist())
-            zip.extract(random_zip_member)
-            print("Extracted", random_zip_member)
+            zip.extract(random_zip_member, "boot.rom")
+            print("Extracted", random_zip_member, "to boot.rom")
 
 
 extract_random(".", "pix")
