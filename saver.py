50e171ed3c5a019f92f0fd3560b69bd6d6024f99
woelper <woelper@gmail.com>
2020-03-03 22:16:54 +0100

    with pattern

--- saver.py
+++ saver.py
@@ -2,9 +2,14 @@ import zipfile
 import os
 import random
 
-archive_name = "pix.zip"
-zipfile = zipfile.ZipFile(archive_name, 'r')
-random_zip_member = random.choice(zipfile.namelist())
-zipfile.extract(random_zip_member)
-print("Extracted", random_zip_member)
+def extract_random(folder, start_pattern):
+    for rom_archive in os.listdir(folder):
+        if rom_archive.startswith(start_pattern):
+            zip = zipfile.ZipFile(rom_archive, 'r')
+            # only .sfc
+            random_zip_member = random.choice(zip.namelist())
+            zip.extract(random_zip_member)
+            print("Extracted", random_zip_member)
 
+
+extract_random(".", "pix")
